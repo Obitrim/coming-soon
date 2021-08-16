aa726a0fdad61497b40e65407ec9fa9b31a45b24
obitrim <pevangelist516@gmail.com>
2021-08-16 19:56:32 +0000

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import "./App.css";
+import React, { useState } from 'react';
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+import AppFooter from "./components/Footer";
+import Features from "./components/Features";
+import Container from "./components/Container";
 
 const App = (props) => {
-  return (
-    <div>App</div>
-  )
+	const [focus, setFocus] = useState(false);
+
+	function handleOnRegister(evt) {
+		evt.preventDefault();
+		setFocus(true);
+	}
+
+	return (
+		<main>
+			<NavBar onRegister={handleOnRegister}/>
+			<Banner subscriptionInputFocus={focus} />
+		    <Features />
+		    <AppFooter />
+		</main>
+	)
 }
 
 export default App;
